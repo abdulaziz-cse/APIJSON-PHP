386994af431d242ac3891cd7ade34f090e352966
Abdulaziz <31155566+abdulazoz@users.noreply.github.com>
2018-06-07 10:52:10 +0300

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::post('user/register', 'APIRegisterController@register');
+
+Route::post('user/login', 'APILoginController@login');
+
+
+Route::middleware('jwt.auth')->get('/users', function (Request $request) {
+    return auth()->user();
+});
+
+
+Route::middleware('jwt.auth')->group( function(){
+    Route::resource('books', 'API\BookController') ;
+} );
